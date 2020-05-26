8914973bf75ab18a100ff3bde057678e6e8460d2
Carlos Tadeu Galeri Astori <tadeu.astori@hotmail.com>
2020-05-26 18:31:53 -0300

    Project Created

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# MostUsedWords
+# Most Used Words
+[![Generic badge](https://img.shields.io/badge/ElectronJs-5.0.0-BLUE.svg)]()
+[![Generic badge](https://img.shields.io/badge/VUE-4.3.0-BRIGHTGREEN.svg)]()
+[![Generic badge](https://img.shields.io/badge/Vuetify-2.2.11-VIOLET.svg)]()
+[![Generic badge](https://img.shields.io/badge/HTML-ORANGE.svg)]()
+[![Generic badge](https://img.shields.io/badge/JavaScript-YELLOW.svg)]()
+[![Generic badge](https://img.shields.io/badge/CSS-LIGHTGREY.svg)]()
+
+<br>
+This app was created as a part of my learning about ElectronJs Framework. I made this desktop app during the "Aplicação Desktop com JavaScript, Electron JS e Vue JS" course in my Udemy Platform account.
+
+> Check the course on [Udemy Course Page](https://www.udemy.com/course/aplicacao-desktop-com-javascript-electron-js-e-vue-js/)
+
+
+## Feature
+Read a .txt file with some text and count how many times a word is repeated inside it.
---
+++ src/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container fluid>
+    <v-form>
+      <v-file-input 
+        label="Selecione as legendas"
+        prepend-icon="mdi-message-text"
+        outlined
+        append-outer-icon="mdi-send"
+        multiple 
+        chips 
+        v-model="files"
+        @click:append-outer="processSubtitles"/>
+    </v-form>
+
+    <div class="pill">
+      <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount" />
+    </div>
+  </v-container>
+</template>
+
+<script>
+import { ipcRenderer } from 'electron'
+import Pill from "./Pill";
+
+export default {
+  components: {
+    Pill
+  },
+  data: function() {
+    return {
+      files: [],
+      groupedWords: []
+    };
+  },
+  methods: {
+    processSubtitles() {
+      const paths = this.files.map(f => f.path)
+      ipcRenderer.send('process-subtitles', paths)
+      ipcRenderer.on('process-subtitles', (event, resp) => {
+        this.groupedWords = resp 
+      })
+    }
+  }
+};
+</script>
+
+<style>
+.pill {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
